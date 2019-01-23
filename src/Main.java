e92091b3538d3abddbf2028f683c34751e65ef58
LeszekS <leszekstepien@o2.pl>
2019-01-23 12:03:54 +0100

    Wersja podstawowa

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,27 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+
+        Scanner sc = new Scanner(System.in);
+        sc.useLocale(Locale.GERMANY);
+
+
+        System.out.println("Podaj temperaturę w pokoju ");
+        double tempWPokoju = sc.nextDouble();
+        sc.nextLine();
+        sc.close();
+
+        Room room = new  Room(45,tempWPokoju,true);
+
+        System.out.println("Temperatura początkowa - " + room.getTemperature());
+        System.out.println("Czy obniżono temp lub czy jest klima ? - " +room.testTemperaturyRoom(room));
+        System.out.println("Temperatura na koniec - " + room.getTemperature());
+
+
+
+    }
 }
--- src/Room.java
+++ src/Room.java
@@ -1,2 +1,57 @@
 public class Room {
+
+    private double size;
+    private double temperature;
+    private boolean airConditioner;
+
+    public Room(double size, double temperature, boolean airConditioner) {
+        this.size = size;
+        this.temperature = temperature;
+        this.airConditioner = airConditioner;
+    }
+
+    public Room() {
+    }
+
+    ;
+
+    public boolean isAirConditioner() {
+        return airConditioner;
+    }
+
+    public void setAirConditioner(boolean airConditioner) {
+        this.airConditioner = airConditioner;
+    }
+
+    public double getSize() {
+        return size;
+    }
+
+    public void setRoom(double room) {
+        this.size = size;
+    }
+
+    public double getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(double temperature) {
+        this.temperature = temperature;
+    }
+
+
+    boolean testTemperaturyRoom(Room room) {
+        final int TEMP_MIN = 15;
+        boolean testTemperatury = true;
+        --room.temperature;
+        if (room.temperature < TEMP_MIN) {
+            room.temperature = TEMP_MIN;
+            testTemperatury = false;
+        }
+        if (!room.airConditioner) {
+            testTemperatury = false;
+        }
+        return testTemperatury;
+    }
+
 }
